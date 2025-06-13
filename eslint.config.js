259240eb75432db4c678e48868eff43471bbaec7
siddannth <sidhantprince72@gmail.com>
2025-06-12 19:15:59 -0500

    Fix case-sensitive import path for ProductCard

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginImport from "eslint-plugin-import";
 
 export default [
   { ignores: ['dist'] },
@@ -19,9 +20,11 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+       import: pluginImport,
     },
     rules: {
       ...js.configs.recommended.rules,
+      "import/no-unresolved": ["error", { caseSensitive: true }],
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
